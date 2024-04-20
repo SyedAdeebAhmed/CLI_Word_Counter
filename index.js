72e6935b28c3ec74e2d30673d6f36402e7e560bb
Syed Adeeb Ahmed <160167608+SyedAdeebAhmed@users.noreply.github.com>
2024-04-20 04:00:23 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let userInput = await inquirer.prompt([
+    {
+        name: "word",
+        type: "input",
+        message: "Enter any sentence or word: "
+    }
+]);
+let word = userInput.word;
+let wordCount = word.split(" ").length;
+console.log(`The Sentence "${word}" has ${wordCount} words`);
